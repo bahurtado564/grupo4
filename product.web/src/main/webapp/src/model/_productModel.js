e36cfdff01eb8c030ac0408fca331e27865e8ce1
admin <admin@ISIS2603-04>
2014-02-15 12:05:42 -0500

    Imagen

--- product.web/src/main/webapp/src/model/_productModel.js
+++ product.web/src/main/webapp/src/model/_productModel.js
@@ -8,7 +8,8 @@ define([], function() {
                   ,  
 		 'descripcion' : ''
                   ,  
-		 'unidades' : ''
+		 'unidades' : '',
+                 'imagen' : ''
         },
         initialize: function() {
         },
