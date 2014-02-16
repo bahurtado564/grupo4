d61ce1689272dcadca3a64ebb3dc66fbb1927bd6
admin <admin@ISIS2603-04>
2014-02-15 22:06:48 -0500

    ParteReqBusqueda1

--- pyme.web/src/main/webapp/src/delegate/pymeDelegate.js
+++ pyme.web/src/main/webapp/src/delegate/pymeDelegate.js
@@ -1,5 +1,17 @@
 define(['delegate/_PymeDelegate'], function() {
     App.Delegate.PymeDelegate = App.Delegate._PymeDelegate.extend({
-        
+        search: function(pyme, callback, callbackError) {
+            console.log('Pyme Search: ');
+            $.ajax({
+                url: '/pyme.service.subsystem.web/webresources/Pyme/search',
+                type: 'POST',
+                data: JSON.stringify(pyme),
+                contentType: 'application/json'
+            }).done(_.bind(function(data) {
+                callback(data);
+            }, this)).error(_.bind(function(data) {
+                callbackError(data);
+            }, this));
+        }
     });
 });
